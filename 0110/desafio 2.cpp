96084bb6b7d4dcd7aee354f3a4346990ece404e7
msnsone <sansone.marcelo@gmail.com>
2022-10-01 11:03:15 -0300

    Update desafio 2.cpp

--- 0110/desafio 2.cpp
+++ 0110/desafio 2.cpp
@@ -1,9 +1,3 @@
-// fazer uum programa em linguagem C o qual produza a seguinte saida
-// 1-10
-// 2-9
-// 3-8
-// ..
-// 10-1 
 
 #include <stdio.h>
 
@@ -13,7 +7,7 @@ char x, y= 'a';
 	
 for ( x= 'A'; x <= 'Z';  x++){
 printf ("%c%c \n", x, y);
-y +=1;
+y ++;
 }
 	
 return 0;	
