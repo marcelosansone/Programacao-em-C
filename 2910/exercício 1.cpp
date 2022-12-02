842b2e60cc8e56df8c6d5fb51c1a10e26c00bebf
msnsone <sansone.marcelo@gmail.com>
2022-12-02 16:53:56 -0300

    Create exercício 1.cpp

---
+++ 2910/exercício 1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int n1,n2,n3
+printf ("Digite um numero");
+scanf("%d", &n1);
+printf("Digite outro número");
+scanf("%i", & n2);
+if n1 > n2 = n3{
+	else
+
+	printf("O maior numero e "%i"",n3
+
+return 0 ;
+	}
+}
